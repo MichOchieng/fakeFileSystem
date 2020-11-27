949b6b492c42c247cf0a930623084a0d98c60fb5
Momo <michochieng@gmail.com>
2020-11-27 00:36:32 -0800

    Added partitioning methods and changed diskWrite

--- diskSim.c
+++ diskSim.c
@@ -1,22 +1,37 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include "headers/diskSim.h"
 
+const int superblockStart = 0;
+const int inodeStart      = 0;
+const int dataGroupStart  = 0;
+
+const int superblockEnd   = 0;
+const int inodeGroupEnd   = 0;
+const int dataGroupEnd    = 0;
+
 unsigned char disk[maxSize];
+char *test = "test";
 
 int main(){
-    diskInit();
-    diskRead(0);
-    diskWrite(0);
+    diskInit();    
+    diskWrite(0,test);
     diskRead(0);
 
     return 0;
 }
 
 void diskInit(){
-    for (size_t i = 0; i < maxSize; i++)
-    {
-        disk[i] = 0;
-    }    
+    // Format the disk
+    formatDisk();    
+    // Create superblock
+    createSuperBlock();
+    // Create Inode bitmap
+    createInodeBitmap();
+    // Create Inodes 
+    createInode();
+    // Create Data groups
+    createBlockGroup();
 }
 
 void diskRead(int index){
@@ -29,28 +44,59 @@ void diskRead(int index){
     {
         int blockStart = index * 128;
         int blockEnd   = blockStart + 128;
-        for (size_t i = blockStart; i < blockEnd; i++)
+        for (int i = blockStart; i < blockEnd; i++)
         {
-            printf("%02x\n",disk[i]);
+            printf("%c\n",disk[i]);
         }  
     }   
       
 }
 
-void diskWrite(int index){
+void diskWrite(int index,char *input){
     if (index > maxBlocks)
     {
         printf("The index entered is too large.");
         exit(0);
     }
-    else
-    {
+    else 
+    {   
+        // Check to see if the block is available
         int blockStart = index * 128;
         int blockEnd   = blockStart + 128;
-        for (size_t i = blockStart; i < blockEnd; i++)
-        {
-            disk[i] = 0xA;        
-        }    
-    }
-    
+
+        // Take in the string
+        // Break it down word by word
+        // Store each char to one position of the block
+        int i = 0;
+        while (input[i] != '\0'){     
+            disk[blockStart] = input[i];      
+            blockStart++;     
+            i++;
+        }
+    }    
+}
+
+void formatDisk(){
+    for (int i = 0; i < maxSize; i++)
+    {
+        disk[i] = 0;
+    }    
+}
+
+void createSuperBlock(){
+    disk[superblockStart]     = magicNum;
+    disk[superblockStart + 1] = maxBlocks;
+    disk[superblockStart + 2] = maxInodes;
+}
+
+void createInodeBitmap(){
+
+}
+
+void createInode(){
+
+}
+
+void createBlockGroup(){
+
 }
