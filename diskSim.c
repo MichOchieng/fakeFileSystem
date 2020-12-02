b46a4d049695191a5df48bb5b54ef604a9279b19
Momo <michochieng@gmail.com>
2020-12-01 22:46:54 -0800

    Added makefile, getFreeBlocka and createInode methods

--- diskSim.c
+++ diskSim.c
@@ -1,20 +1,23 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <stdbool.h>
 #include "headers/diskSim.h"
 
 unsigned char disk[maxSize];
+char *fileNames[nInodes];
 
 void diskInit(){
     // Format the disk
     formatDisk();    
     // Create superblock
     createSuperBlock();   
+    printf("--Disk created\n");
 }
 
 void diskRead(int startBlock,int nBlocks){
     if (startBlock > maxBlocks) // Outside the range of blocks
     {
-        printf("The index entered is too large.");
+        printf("--The index entered is too large.");
         exit(0);
     }
     else
@@ -60,12 +63,64 @@ void formatDisk(){
     }   
 }
 
+void makeFile(int index,char *filename){ 
+    fileNames[index] = filename;   
+    int inodeIndex   = getFreeInode();
+    createInode(inodeIndex);    
+}
+void writeFile(char *filename,char *str){
+    // Find inode index
+    // Find next free block
+    // Set inode pointer to block
+        // If block is full -> grab new block
+            // If 4 blocks taken -> indirect pointer
+    
+}
+
 void createSuperBlock(){
     disk[superblockStart]     = magicNum;
     disk[superblockStart + 1] = maxBlocks;
     disk[superblockStart + 2] = maxInodes;
 }
 
-void createInode(){
+void createInode(int index){    
+    int fileSize   = 0;
+    disk[index + 1] = fileSize;
+}
+int findFile(char *filename){
 
+}
+
+int getFreeInode(){    
+    for (int i = inodeBitmapStart; i <= inodeBitmapEnd; i++)
+    {
+        if (disk[i] == 0)
+        {
+            // Free inode found
+            disk[i] = 1;
+            printf("--Free inode at index: %i\n",i);
+            return i;
+        }
+        else if(i == inodeBitmapEnd && disk[i] == 1)
+        {
+            printf("--Max files reached.\n");
+        }       
+    }    
+}
+
+int getFreeBlock(){
+    for (int i = dataBitmapStart; i <= dataBitmapEnd; i++)
+    {
+        if (disk[i] == 0)
+        {
+            // Free inode found
+            disk[i] = 1;
+            printf("--Free data block at index: %i\n",i);
+            return i;
+        }
+        else if(i == inodeBitmapEnd && disk[i] == 1)
+        {
+            printf("--.\n");
+        }       
+    }
 }
